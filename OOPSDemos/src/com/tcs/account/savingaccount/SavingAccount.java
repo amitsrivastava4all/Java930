7d5ea99de712b2f65154c86e10d3b58d63cce807
amit.srivastava4all@gmail.com <amit.srivastava4all@gmail.com>
2015-08-28 15:48:51 +0530

    Package Demo

---
+++ OOPSDemos/src/com/tcs/account/savingaccount/SavingAccount.java
@@ -0,0 +1,13 @@
+package com.tcs.account.savingaccount;
+
+import com.tcs.insurance.lic.Insurance;
+
+public class SavingAccount {
+	
+	public static void main(String[] args) {
+		Insurance insurance = new Insurance();
+		//insurance.calculatePremium();
+		insurance.applyForInsurance();
+	}
+
+}
---
+++ OOPSDemos/src/com/tcs/insurance/lic/Insurance.java
@@ -0,0 +1,11 @@
+package com.tcs.insurance.lic;
+
+public class Insurance {
+
+	public void applyForInsurance(){
+		System.out.println("Apply For Insurance");
+	}
+	void calculatePremium(){
+		System.out.println("This is not visible outside this package");
+	}
+}
